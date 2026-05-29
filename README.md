c92c90a61c62c3270b3cb849875c84835f1995c0
Helen <heletjhb025@student.wethinkcode.co.za>
2026-05-29 17:09:44 +0200

    feat: create Transaction class with validation and toString

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Smart-Budget-Tracker
+1. Define the problem
+2. What should this app do?
+3. This app should allow the user to:
+   4. add income
+   5. add expenses
+   6. view transaction history
+   7. save data
---
+++ src/model/Transaction.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Transaction {
+    private double amount;
+    private String type;
+    private String date;
+    private String category;
+    private String description;
+
+    public Transaction(double amount, String type, String date, String category, String description){
+        if(amount < 0){
+            throw new IllegalArgumentException("amount cannot be less than 0");
+        }
+        this.amount = amount;
+        this.type =type;
+        this.date = date;
+        this.category = category;
+        this.description = description;
+    }
+
+    //GETTERS - these allow us to access the fields since they are private
+    public double getAmount(){
+        return amount;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setAmount(double amount) {
+        if(amount < 0) {
+            throw new IllegalArgumentException("amount cannot be less than 0");
+        }
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return getDate() + " | " + getType() + " | " + getCategory() + " | " + getAmount() + " | " + getDescription();
+    }
+
+
+
+}
